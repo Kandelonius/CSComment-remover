fc336ed2519f866b16be5b7167758b94bd83d917
Shane Kaestner <shane.kaestner@yahoo.com>
2022-06-13 19:00:20 -0700

    now have the program creating the new string with new lines after each line and automatically adding single digit lines to the output line.

--- CSCommentRemover/Program.cs
+++ CSCommentRemover/Program.cs
@@ -16,28 +16,33 @@ namespace CSCommentRemover
             //Application.Run(new Form1());
             if (Clipboard.ContainsText())
             {
+
                 String text = Clipboard.GetText(); // pulls the text from the clipboard to remove single line comments from
                 String[] lines = text.Split(new String[] { Environment.NewLine }, StringSplitOptions.None); // splits the lines of the text so we can look line by line
-                String[] newLines = new string[];
-                int index = 0;
+                String newLines = "";
+                Console.WriteLine("lines is " + lines.Length);
+                //int index = 0;
                 foreach (String line in lines)
                 {
                     String newLine = line.TrimStart();
-                    if (newLine.Length > 1)
+                    if (newLine.Length >= 2)
                     {
                         char charOne = newLine[0];
                         char charTwo = newLine[1];
                         if (!(charOne == '/') && !(charTwo == '/'))
                         {
-                            Console.WriteLine("one is " + charOne + " and two is " + charTwo);
-                            newLines.Append(newLine)
-                            //Console.WriteLine(line.Trim() + index);
-                            //index++;
+                            newLines += newLine;
+                            newLines += Environment.NewLine;
                         }
-                        //Console.WriteLine(line.Trim() + index);
+                    } else if (newLine.Length > 0)
+                    {
+                        newLines += newLine;
+                        newLines += Environment.NewLine;
                     }
                 }
+                newLines = newLines.Substring(0, newLines.Length - 1); // removes the last empty line
                 Console.WriteLine(text);
+                Console.WriteLine("newLines is " + newLines);
             }
             else
             {
