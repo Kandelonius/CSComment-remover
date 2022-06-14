34cc7486f5caacdcc2706ea1c6b8f01be4d83f79
Shane Kaestner <shane.kaestner@yahoo.com>
2022-06-13 22:10:28 -0700

    moved the logic for the comment removal to a seperate class.

--- CSCommentRemover/Program.cs
+++ CSCommentRemover/Program.cs
@@ -16,40 +16,19 @@ namespace CSCommentRemover
             //Application.Run(new Form1());
             if (Clipboard.ContainsText())
             {
-
                 String text = Clipboard.GetText(); // pulls the text from the clipboard to remove single line comments from
-                String[] lines = text.Split(new String[] { Environment.NewLine }, StringSplitOptions.None); // splits the lines of the text so we can look line by line
-                String newLines = "";
-                Console.WriteLine("lines is " + lines.Length);
-                //int index = 0;
-                foreach (String line in lines)
-                {
-                    String newLine = line.TrimStart();
-                    if (newLine.Length >= 2)
-                    {
-                        char charOne = newLine[0];
-                        char charTwo = newLine[1];
-                        if (!(charOne == '/') && !(charTwo == '/'))
-                        {
-                            newLines += newLine;
-                            newLines += Environment.NewLine;
-                        }
-                    } else if (newLine.Length > 0)
-                    {
-                        newLines += newLine;
-                        newLines += Environment.NewLine;
-                    }
-                }
-                newLines = newLines.Substring(0, newLines.Length - 1); // removes the last empty line
-                Console.WriteLine(text);
-                Console.WriteLine("newLines is " + newLines);
+                String newLines = RemoveCommentsAndEmptyLines.RemoveComments(text);
+                Clipboard.SetText(newLines);
+
+                Console.WriteLine("original test is \n" + text);
+                Console.WriteLine("newLines is \n" + newLines);
             }
             else
             {
                 Console.WriteLine("No text");
             }
-                
+            
+
         }
     }
 }
-
---
+++ CSCommentRemover/RemoveCommentsAndEmptyLines.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCommentRemover
+{
+    static class RemoveCommentsAndEmptyLines
+    {
+        public static string RemoveComments(string input)
+        {
+            String[] lines = input.Split(new String[] { Environment.NewLine }, StringSplitOptions.None); // splits the lines of the text so we can look line by line
+            String newLines = "";
+            Console.WriteLine("lines is " + lines.Length);
+            //int index = 0;
+            foreach (String line in lines)
+            {
+                String newLine = line.TrimStart();
+                if (newLine.Length >= 2)
+                {
+                    char charOne = newLine[0];
+                    char charTwo = newLine[1];
+                    if (!(charOne == '/') && !(charTwo == '/'))
+                    {
+                        newLines += newLine;
+                        newLines += Environment.NewLine;
+                    }
+                }
+                else if (newLine.Length == 1)
+                {
+                    newLines += newLine;
+                    newLines += Environment.NewLine;
+                }
+            }
+            newLines = newLines.Substring(0, newLines.Length - 2); // removes the last empty line
+            return newLines;
+        }
+    }
+}
